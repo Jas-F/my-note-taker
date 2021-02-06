2a0523904fe64d72a22ee92e38b837c52f8b36c9
Jas-F <jasminer.franklin@gmail.com>
2021-02-05 20:18:06 -0500

    add route to notes.html

--- routes/html.js
+++ routes/html.js
@@ -1,7 +1,13 @@
 var router = require("express").Router();
 var path = require("path");
 
+// route to index
 router.get("/", (req, res) => {
 res.sendFile(path.join(__dirname, "../public/index.html"));
 
+});
+
+// route to notes
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 })
