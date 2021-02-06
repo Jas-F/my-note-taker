09c7c06e4eb2748095e046dc9d4538ac4ec84712
Jas-F <jasminer.franklin@gmail.com>
2021-02-05 19:57:02 -0500

    add alert

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000
 
+// server middleware pros
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+
+
+
+
+// add alert for successful port connection
+app.listen(PORT,() => {
+console.log("port complete")
+})
