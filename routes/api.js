34a321d277bd91e3a66e32faa59f6319bba8e070
Jas-F <jasminer.franklin@gmail.com>
2021-02-06 02:04:00 -0500

    add heroku

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,21 @@ app.get("/api/notes", (req, res) => {
 });
 
 // create path for saving notes
-app.post("/api/notes", (req, res) => {
-    return res.json(note)
-});
+// app.post("/api/notes", function(req, res) {
+//     // req.body hosts is equal to the JSON post sent from the user
+//     // This works because of our body parsing middleware
+//     var newNote = .;
+  
+//     // Using a RegEx Pattern to remove spaces from newCharacter
+//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//     newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+//     console.log(newcharacter);
+  
+//     characters.push(newcharacter);
+  
+//     res.json(newcharacter);
+//   });
 
 // create path for deleting notes
 app.delete("/notes", (req, res) => {
