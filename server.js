ba85d4243cdbaccae956302bb329e686d529d882
Jas-F <jasminer.franklin@gmail.com>
2021-02-05 20:01:21 -0500

    insert routes

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-
-
-
-
+// require both routes
+var api = require("./routes/api")
+var html = require("./routes/html")
+app.use(api)
+app.use(html)
 
 // add alert for successful port connection
 app.listen(PORT,() => {
